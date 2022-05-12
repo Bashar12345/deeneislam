9ea326ac20d0061764dc7936655d9cb433e2e13d
Muhammad Abul Bashar <45920385+Bashar12345@users.noreply.github.com>
2022-05-12 16:28:26 +0600

    init

--- cfehome/settings.py
+++ cfehome/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 import os
 from pathlib import Path
+import django_heroku
 
 
 
@@ -149,3 +150,5 @@ MEDIA_URL ='/media/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
