25ff6068c3f72b336f9a95ba6d69e3fd424b7a9b
Muhammad Abul Bashar <45920385+Bashar12345@users.noreply.github.com>
2022-05-12 15:51:54 +0600

    Updated Static root

--- cfehome/settings.py
+++ cfehome/settings.py
@@ -124,16 +124,19 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-print(PROJECT_ROOT)
-#
-STATIC_ROOT = os.path.join(PROJECT_ROOT,'static')
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
+STATIC_URL = 'static/'
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+
+#print(PROJECT_ROOT)
+#
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
     
-)
+# )
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
