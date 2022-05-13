a24941548efeaaec8d732e9791a45c0caf8bd97a
Muhammad Abul Bashar <45920385+Bashar12345@users.noreply.github.com>
2022-05-13 15:16:37 +0600

    static heroku updates

--- cfehome/settings.py
+++ cfehome/settings.py
@@ -138,9 +138,9 @@ STATIC_URL = 'static/'
 #
 
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
