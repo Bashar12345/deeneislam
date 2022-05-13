aa88134f9462412587d05c164e45dc9871d2886f
Muhammad Abul Bashar <45920385+Bashar12345@users.noreply.github.com>
2022-05-13 15:18:34 +0600

    static heroku updates

--- cfehome/settings.py
+++ cfehome/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 
 
 
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = 'static/'
 
 #PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
