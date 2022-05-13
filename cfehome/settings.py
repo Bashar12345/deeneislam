4730fb7cec796526daf61f926f42887f17bd419c
Muhammad Abul Bashar <45920385+Bashar12345@users.noreply.github.com>
2022-05-13 16:14:26 +0600

    init

--- cfehome/settings.py
+++ cfehome/settings.py
@@ -136,9 +136,9 @@ STATIC_URL = 'static/'
 #PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 #print(PROJECT_ROOT)
 
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, 'static'),
-# )
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
