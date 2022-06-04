eb8eafd46ddc6de5717a28b794d75f1cf411ee70
Bashar12345 <abul35-2002@diu.edu.bd>
2022-06-04 12:52:34 +0600

    init

--- RED/api_data_sets.py
+++ RED/api_data_sets.py
@@ -4,6 +4,7 @@ from datetime import datetime, timedelta, date
 import requests
 import json
 
+
 # Init
 newsapi_Api_key='0bf80e3a6a5d4fefb6b80ceeaccb9560'
 
@@ -30,8 +31,23 @@ parameters = {
     'apiKey': newsapi_Api_key # your own API key
 }
 
-# if top_headlines:
-#     print(top_headlines)
+
+def get_quran_translation_list():
+    raw_response='https://quranenc.com/api/v1/translations/list'
+
+
+
+def get_selected_quran_surah_with_translation():
+    raw_response='https://quranenc.com/api/v1/translation/sura/english_saheeh/1'
+
+def qurans_bangla():
+    raw_responsce='http://api.alquran.cloud/v1/quran'
+
+#https://www.hadithapi.com/api/sahih-bukhari/chapters?apiKey=$2y$10$kjKW8PvRxeTN5TKbRSuaug8HbfPKgr3Qe6Fin6A9UiO4FwsIVfNu
+#https://api.hadith.sutanlab.id/books/bukhari?range=1-150
+def haddit():
+    return 0
+
 
 
  # /v2/top-headlines
@@ -56,6 +72,11 @@ def top_headlines():
     headlines =  newsapi_raw_response.json()
     #print(headlines)
     return headlines['articles']
+# if top_headlines:
+#     print(top_headlines)
+
+
+
 
 
 # /v2/everything
