1854722ded307adc3b613775bbbb84b1db947a28
Muhammad Abul Bashar <45920385+Bashar12345@users.noreply.github.com>
2022-06-06 22:31:01 +0600

    init

--- RED/utils.py
+++ RED/utils.py
@@ -131,64 +131,58 @@ def news_page():
         urlToImage.append(article['urlToImage']) 
         publishedAt.append(article['publishedAt']) 
     
-    # for t in title:
-    #     print(t)
     article_list = itertools.zip_longest(source, title, description, url, urlToImage, publishedAt, content)
     context = {'article_list':article_list}
-
+    # for t in title:
+    #     print(t)
     return context
 
 
+'''"results": [
+        {
+            "title": "Gyanvapi case: Muslim side to continue arguments today",
+            "link": "https://www.orissapost.com/gyanvapi-case-muslim-side-to-continue-arguments-today/",
+            "keywords": [
+                "National",
+                "Prime News",
+                "Gyanvapi",
+                "Gyanvapi mosque",
+                "Muslims"
+            ],
+            "creator": [
+                "Post News Network"
+            ],
+            "video_url": null,
+            "description": "Varanasi: Advocates for the Anjuman Intezamia Masjid (AIM), the Muslim side in the Gyanvapi mosque dispute, will continue their arguments on Monday challenging the maintainability of case. “In the May 26 hearing, our team had explained how the claims and demands of the women plaintiffs cannot be allowed as they had not produced any evidence […]",
+            "content": "Varanasi: Advocates for the Anjuman Intezamia Masjid (AIM), the Muslim side in the Gyanvapi mosque dispute, will continue their arguments on Monday challenging the maintainability of case. “In the May 26 hearing, our team had explained how the claims and demands of the women plaintiffs cannot be allowed as they had not produced any evidence in support of their petition while their averments in several paragraphs of their petition are contradictory,” AIM advocate Abhay Nath Yadav said. On May 26, the district judge had started hearing the case on the issue of its maintainability in compliance with the order of the Supreme Court. The plaintiffs’ lawyer Hari Shankar Jain said his team will be in court on Monday though he is not sure whether the arguments of AIM advocates would conclude, and they would get an opportunity to counter them. AIM advocates are also likely to mention how the Gyanvapi mosque is covered under the Protection of Places of Worship Act, 1991, besides other laws and orders of the Supreme Court. Plaintiffs Rakhi Singh of Delhi and Laxmi Devi, Sita Sahu, Manju Vyas and Rekha Pathak had filed the petition in the court of civil judge on August 18, 2021. On April 8, the court appointed an advocate commissioner for the survey and videography of the Gyanvapi mosque.",
+            "pubDate": "2022-05-30 04:46:56",
+            "image_url": "https://www.orissapost.com/wp-content/uploads/2022/05/Gyanvapi-287x300.jpg",
+            "source_id": "orissapost",
+            "country": [
+                "india"
+            ],
+            "category": [
+                "top"
+            ],
+            "language": "english"
+        },
 
 
+news_api
 
 
-
-# "results": [
-    #     {
-    #         "title": "Gyanvapi case: Muslim side to continue arguments today",
-    #         "link": "https://www.orissapost.com/gyanvapi-case-muslim-side-to-continue-arguments-today/",
-    #         "keywords": [
-    #             "National",
-    #             "Prime News",
-    #             "Gyanvapi",
-    #             "Gyanvapi mosque",
-    #             "Muslims"
-    #         ],
-    #         "creator": [
-    #             "Post News Network"
-    #         ],
-    #         "video_url": null,
-    #         "description": "Varanasi: Advocates for the Anjuman Intezamia Masjid (AIM), the Muslim side in the Gyanvapi mosque dispute, will continue their arguments on Monday challenging the maintainability of case. “In the May 26 hearing, our team had explained how the claims and demands of the women plaintiffs cannot be allowed as they had not produced any evidence […]",
-    #         "content": "Varanasi: Advocates for the Anjuman Intezamia Masjid (AIM), the Muslim side in the Gyanvapi mosque dispute, will continue their arguments on Monday challenging the maintainability of case. “In the May 26 hearing, our team had explained how the claims and demands of the women plaintiffs cannot be allowed as they had not produced any evidence in support of their petition while their averments in several paragraphs of their petition are contradictory,” AIM advocate Abhay Nath Yadav said. On May 26, the district judge had started hearing the case on the issue of its maintainability in compliance with the order of the Supreme Court. The plaintiffs’ lawyer Hari Shankar Jain said his team will be in court on Monday though he is not sure whether the arguments of AIM advocates would conclude, and they would get an opportunity to counter them. AIM advocates are also likely to mention how the Gyanvapi mosque is covered under the Protection of Places of Worship Act, 1991, besides other laws and orders of the Supreme Court. Plaintiffs Rakhi Singh of Delhi and Laxmi Devi, Sita Sahu, Manju Vyas and Rekha Pathak had filed the petition in the court of civil judge on August 18, 2021. On April 8, the court appointed an advocate commissioner for the survey and videography of the Gyanvapi mosque.",
-    #         "pubDate": "2022-05-30 04:46:56",
-    #         "image_url": "https://www.orissapost.com/wp-content/uploads/2022/05/Gyanvapi-287x300.jpg",
-    #         "source_id": "orissapost",
-    #         "country": [
-    #             "india"
-    #         ],
-    #         "category": [
-    #             "top"
-    #         ],
-    #         "language": "english"
-    #     },
-
-
-# news_api
-
-
-# "articles": [
-#         {
-#             "source": {
-#                 "id": "la-nacion",
-#                 "name": "La Nacion"
-#             },
-#             "author": null,
-#             "title": "La China Suárez denunció a Ángel de Brito por hostigamiento y recibió una tajante respuesta al aire - LA NACION",
-#             "description": "La actriz denunció malos tratos por parte de la prensa del espectáculo; Yanina Latorre se sumó a las críticas y la desafió",
-#             "url": "https://www.lanacion.com.ar/espectaculos/television/la-china-suarez-acuso-a-angel-de-brito-de-hostigamiento-y-recibio-una-tajante-respuesta-al-aire-nid25052022/",
-#             "urlToImage": "https://resizer.glanacion.com/resizer/u-kJqonM5IvlrnVOsdffPxRhPZc=/768x0/filters:format(webp):quality(80)/cloudfront-us-east-1.images.arcpublishing.com/lanacionar/JWI2FMV2TRBKVDBCFFV7G4F7IY.jpg",
-#             "publishedAt": "2022-05-25T04:36:00Z",
-#             "content": "No es la primera vez que María Eugenia La China Suárez hace referencia al hostigamiento que siente por un sector de la prensa de espectáculos. En reiteradas oportunidades, la actriz destacó el ensaña… [+3579 chars]"
-#         },
-# ]
+"articles": [
+        {
+            "source": {
+                "id": "la-nacion",
+                "name": "La Nacion"
+            },
+            "author": null,
+            "title": "La China Suárez denunció a Ángel de Brito por hostigamiento y recibió una tajante respuesta al aire - LA NACION",
+            "description": "La actriz denunció malos tratos por parte de la prensa del espectáculo; Yanina Latorre se sumó a las críticas y la desafió",
+            "url": "https://www.lanacion.com.ar/espectaculos/television/la-china-suarez-acuso-a-angel-de-brito-de-hostigamiento-y-recibio-una-tajante-respuesta-al-aire-nid25052022/",
+            "urlToImage": "https://resizer.glanacion.com/resizer/u-kJqonM5IvlrnVOsdffPxRhPZc=/768x0/filters:format(webp):quality(80)/cloudfront-us-east-1.images.arcpublishing.com/lanacionar/JWI2FMV2TRBKVDBCFFV7G4F7IY.jpg",
+            "publishedAt": "2022-05-25T04:36:00Z",
+            "content": "No es la primera vez que María Eugenia La China Suárez hace referencia al hostigamiento que siente por un sector de la prensa de espectáculos. En reiteradas oportunidades, la actriz destacó el ensaña… [+3579 chars]"
+        },
+]'''
