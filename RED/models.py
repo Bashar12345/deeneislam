9c99815b2f2c300d442a7849d2d4f635e4a875d0
Bashar12345 <abul35-2002@diu.edu.bd>
2022-06-07 16:15:45 +0600

    init

--- RED/models.py
+++ RED/models.py
@@ -14,9 +14,14 @@ class chapter_ayats(models.Model):
     chapter_id = models.IntegerField(blank=False,null=False)
     chapter_name = models.CharField(max_length=100)
     #ayats = models.JSONField(default=dict) # for list of ayats 
+    def __str__(self):
+        return f'{self.chapter_name} '
+
 
 class qurans_ayats(models.Model):
     chapter_id = models.ForeignKey(chapter_ayats, on_delete=models.CASCADE)  # mField(blank=False,null=False)
-    sura_no = models.IntegerField(blank=False,null=False)
-    surah_name = models.CharField(max_length=24)
-    ayat_no = models.IntegerField(blank=False) 
+    sura_no = models.IntegerField(blank=False,null=False,default=False)
+    surah_name = models.CharField(max_length=24,default=None)
+    ayat_no = models.CharField(max_length=100,null=False)
+    def __str__(self):
+        return f'Surah:-{self.surah_name}, ayat_no:- {self.ayat_no}'
