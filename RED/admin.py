36968ad2d08cff5577a38efa47ca3a6736ee2f56
Bashar12345 <abul35-2002@diu.edu.bd>
2022-06-07 15:16:39 +0600

    init

--- RED/admin.py
+++ RED/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import articles,qurans_ayats
+from .models import articles,qurans_ayats,chapter_ayats
 
 # Register your models here.
 admin.site.register(articles)
+admin.site.register(chapter_ayats)
 admin.site.register(qurans_ayats)
+
